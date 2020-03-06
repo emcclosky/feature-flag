bc12e2f8d8dbb7ab4ccc4c9a6f925cfd07544686
Liz Arinder <elizabeth.m.arinder@gmail.com>
2020-03-06 10:04:47 -0500

    Removing path from checkout in yml

--- circleci.js
+++ circleci.js
@@ -4,6 +4,7 @@ const { exec } = require('child_process');
 const modules = ['server'];
 
 exec('npx lerna changed --all', (error, stdout) => {
+    console.log('BEGINNING CIRCLECI.JS')
     modules.forEach(name => {
         if (!stdout.includes(name)) return;
 
